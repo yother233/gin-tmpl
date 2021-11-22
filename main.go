78a53b81a94c8fd2a63192589aef59b7b41eb808
Yother <Yother233@gmail.com>
2021-11-22 22:20:36 +0800

    Pass

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"gin-tmp/core"
+	"fmt"
+	"gin-tmpl/constant/global"
+	"gin-tmpl/internal/pkg/db"
+	"gin-tmpl/internal/pkg/viper"
+	"gin-tmpl/internal/pkg/zap"
 )
 
 func main() {
-	core.Start()
+	// core.Start()
+	// 配置读取
+	var v viper.Viper
+	err := v.InitViper()
+	if err != nil {
+		panic(fmt.Sprintf("配置读取错误：%s", err.Error()))
+	}
+	// 日志配置
+	z := zap.Logger{}
+	z.InitLogger()
+	// 数据库连接
+	g := db.GtGorm{}
+	db, err := g.InitGorm()
+	if err != nil {
+		panic(fmt.Sprintf("配置读取错误：%s", err.Error()))
+	}
+	if err := g.InitMigrate(db); err != nil {
+		if d, err := db.DB(); err != nil {
+			defer d.Close()
+		}
+	} else {
+		global.GtLogger.Info("数据库迁移完成")
+	}
+	// gin服务注册
+
 }
